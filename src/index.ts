51b2b798e62c80239ff39cd67471e964d5f62d46
Collin Holthaus <collinh89@gmail.com>
2023-03-22 18:01:01 -0500

    simplified Nodejs webapi

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,48 @@
-/**
- * Required External Modules
- */
-import * as dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import { usersRouter } from "./users/users.router";
-import { errorHandler } from "./middleware/error.middleware";
-import { notFoundHandler } from "./middleware/not-found.middleware";
+import http from "http";
+import express, { Express } from "express";
+import morgan from "morgan";
+import routes from "./routes/posts";
 
-dotenv.config();
+const router: Express = express();
 
-/**
- * App Variables
- */
-if (!process.env.PORT) {
-  process.exit(1);
-}
+/** Logging */
+router.use(morgan("dev"));
+/** Parse the request */
+router.use(express.urlencoded({ extended: false }));
+/** Takes care of JSON data */
+router.use(express.json());
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
-
-const app = express();
-
-/**
- *  App Configuration
- */
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use("/api/users", usersRouter);
-
-app.use(errorHandler);
-app.use(notFoundHandler);
-
-/**
- * Server Activation
- */
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+/** RULES OF OUR API */
+router.use((req, res, next) => {
+  // set the CORS policy
+  res.header("Access-Control-Allow-Origin", "*");
+  // set the CORS headers
+  res.header(
+    "Access-Control-Allow-Headers",
+    "origin, X-Requested-With,Content-Type,Accept, Authorization"
+  );
+  // set the CORS method headers
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+    return res.status(200).json({});
+  }
+  next();
 });
+
+/** Routes */
+router.use("/", routes);
+
+/** Error handling */
+router.use((req, res, next) => {
+  const error = new Error("not found");
+  return res.status(404).json({
+    message: error.message,
+  });
+});
+
+/** Server */
+const httpServer = http.createServer(router);
+const PORT: any = process.env.PORT ?? 6060;
+httpServer.listen(PORT, () =>
+  console.log(`The server is running on port ${PORT}`)
+);
--- src/users/user.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface BaseUser {
-  name: string;
-  auth0Id: string;
-}
-
-export interface User extends BaseUser {
-  id: number;
-}
